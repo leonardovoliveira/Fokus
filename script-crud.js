dcbc6bd0537cf81a6349f15bceb7e76e7a6f4342
Leonardo Oliveira <leonardo.viegaso@gmail.com>
2024-02-12 12:44:12 -0300

    update

--- script-crud.js
+++ script-crud.js
@@ -4,8 +4,11 @@
  const textArea = document.querySelector('.app__form-textarea')
  const ulTarefas = document.querySelector('.app__section-task-list')
  const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
+ const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+let tarefaSelecionada = null
+let liTarefaSelecionada = null
 
 const limparFormulario = () =>{
    textArea.value = ''
@@ -53,6 +56,22 @@ function criarTarefa(tarefa) {
    li.append(svg)
    li.append(paragrafo)
    li.append(botao)
+   
+   li.onclick = () => {
+      document.querySelectorAll('.app__section-task-list-item-active').forEach(elemento =>{
+         elemento.classList.remove('app__section-task-list-item-active')
+      })
+      if(tarefaSelecionada == tarefa){
+         paragrafoDescricaoTarefa.textContent = ''
+         tarefaSelecionada = null
+         liTarefaSelecionada = null
+         return
+      }
+      tarefaSelecionada = tarefa
+      liTarefaSelecionada = li
+      paragrafoDescricaoTarefa.textContent = tarefa.descricao
+      li.classList.add('app__section-task-list-item-active')
+   }
 
    return li
 }
@@ -76,4 +95,12 @@ function criarTarefa(tarefa) {
  tarefas.forEach(tarefa => {
    const elementoTarefa = criarTarefa(tarefa)
    ulTarefas.append(elementoTarefa)
+ })
+
+ document.addEventListener('FocoFinalizado', () =>{
+   if(tarefaSelecionada && liTarefaSelecionada){
+      liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+      liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+      liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+   }
  })
